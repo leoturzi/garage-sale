838ac8415d8377778d927f6c4c35cb3b806cc1aa
Leo Turzi <leogturzi@gmail.com>
2023-02-26 18:38:27 -0700

    Implement body-parser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const routeHandlers = require('./routes');
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
