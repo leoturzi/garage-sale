e7ebbacbb754b3db271616bcf0fda32d761c8f65
Leo Turzi <leogturzi@gmail.com>
2023-03-02 19:15:19 -0700

    Implemented multer for products

--- controllers/products.js
+++ controllers/products.js
@@ -1,21 +1,74 @@
+const db = require('../models');
+
 const controller = {
-    getProducts: (req, res) => {
-        res.status(200).json({
-            message: 'Products were fetched',
-        });
+    getProducts: async (req, res) => {
+        try {
+            const products = await db.Product.findAll();
+            res.status(200).json({
+                products,
+                message: 'Products were fetched',
+            });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                message: 'Something went wrong',
+            });
+        }
     },
 
-    getProduct: (req, res) => {
-        res.status(200).json({
-            message: 'Product details',
-            productId: req.params.id,
-        });
+    getProduct: async (req, res) => {
+        try {
+            const product = await db.Product.findByPk(req.params.id);
+
+            if (!product) {
+                return res.status(404).json({
+                    message: 'Product not found',
+                });
+            }
+            res.status(200).json({
+                product,
+                message: 'Product details',
+            });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                message: 'Something went wrong',
+            });
+        }
     },
 
-    createProduct: (req, res) => {
-        res.status(201).json({
-            message: 'Product was created',
-        });
+    createProduct: async (req, res) => {
+        // TODO - validate req.body
+
+        try {
+            const product = await db.Product.create({
+                name: req.body.name,
+                price: req.body.price,
+                description: req.body.description,
+                image: JSON.stringify(req.files),
+                discount: req.body.discount,
+                condition: req.body.condition,
+                category: req.body.category,
+            });
+
+            return res.status(201).json({
+                message: 'Product created',
+                product: {
+                    name: req.body.name,
+                    price: req.body.price,
+                    description: req.body.description,
+                    image: req.files,
+                    discount: req.body.discount,
+                    condition: req.body.condition,
+                    category: req.body.category,
+                },
+            });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                message: 'Something went wrong',
+            });
+        }
     },
 
     updateProduct: (req, res) => {
--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers')['products'];
+const uploadFiles = require('../middleware')['multer'];
 
 router.get('/', controller.getProducts);
 
 router.get('/:id', controller.getProduct);
 
-router.post('/', controller.createProduct);
+router.post('/', uploadFiles, controller.createProduct);
 
 router.put('/:id', controller.updateProduct);
 
